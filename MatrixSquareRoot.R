4043cec86fe92fca673d0e0ae18e4969ee490144
hoha_rejection <lim23@vcu.edu>
2022-04-22 23:26:16 -0400

    Update MatrixSquareRoot.R

--- MatrixSquareRoot.R
+++ MatrixSquareRoot.R
@@ -1,18 +1,24 @@
-# randomly generate a positive definite matrix via convariance matrix
-A <- matrix(runif(3^2)*2-1, ncol=n) 
-A <- t(A) %*% A
-A
-
-
-# find square root of matrix A
+#--------- Find square root of matrix A
 # reference from https://stackoverflow.com/questions/28227111/square-root-of-a-singular-matrix-in-r
-tmp = eigen(A)
-T = tmp$vectors
 
-J = diag(tmp$values)
-Tinv = solve(T)
-Jsqrt = diag(sqrt(tmp$values))
-Asqrt = T %*% Jsqrt %*% Tinv
+matrix.sqrt = function(A){
+  require(matrixcalc)
+  if(!is.positive.definite(A)){stop("Please input positive definite matrix.")}
+  
+  # eigen decomposition
+  tmp = eigen(A)
+  T = tmp$vectors
+  J = diag(tmp$values)
+  Tinv = solve(T)
+  
+  Jsqrt = diag(sqrt(tmp$values))
+  Asqrt = T %*% Jsqrt %*% Tinv
+  
+  return(Asqrt)
+}
 
+A = matrix( c( 2, -1, 0, -1, 2, -1, 0, -1, 2 ), nrow=3, byrow=TRUE )
+A
+Asqrt = matrix.sqrt(A)
 # check
 Asqrt %*% Asqrt
